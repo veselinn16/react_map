9fd403afb2977f9ad1cb4b6db437f21e7ee88a51
veselinn16 <veselinn16@gmail.com>
2018-07-25 21:34:32 +0300

    Display a map on the page

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import { withGoogleMap, GoogleMap } from 'react-google-maps';
 
 class Map extends Component {
   render() {
+    const GoogleMapExample = withGoogleMap(props => (
+      <GoogleMap defaultCenter={{ lat: 42.149488, lng: 24.749169 }} defaultZoom={ 13 }></GoogleMap>
+    ));
+
     return (
-      <div>Map Template</div>
+      <div id='map'>
+        <GoogleMapExample containerElement={ <div style={{ height: `100%`, width: `100%` }}/> } mapElement={ <div style={{ height: `100%` }}/> }/>
+      </div>
     );
   }
 };
