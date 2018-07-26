cf3d4ea83e89bb882343ab3797afbcde45161c5f
veselinn16 <veselinn16@gmail.com>
2018-07-26 14:39:50 +0300

    Place a div for list view on the page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Map from './components/Map.js';
+import Sidebar from './components/Sidebar.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Sidebar/>
         <Map/>
       </div>
     );
