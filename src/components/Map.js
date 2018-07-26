8d1ed0c685a6cf9d0944fde8982fb4858cef8194
veselinn16 <veselinn16@gmail.com>
2018-07-26 14:26:02 +0300

    Place 5 locations on map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
-import { withGoogleMap, GoogleMap } from 'react-google-maps';
+import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
 
 class Map extends Component {
   render() {
-    const GoogleMapExample = withGoogleMap(props => (
-      <GoogleMap defaultCenter={{ lat: 42.149488, lng: 24.749169 }} defaultZoom={ 13 }></GoogleMap>
+    const MapComponent = withGoogleMap(props => (
+      <GoogleMap defaultCenter={{ lat: 42.144953, lng: 24.746555 }} defaultZoom={ 16 }>
+        { props.isMarkerShown && <Marker position={{ lat: 42.146252, lng: 24.748833 }} /> }
+        { props.isMarkerShown && <Marker position={{ lat: 42.140500, lng: 24.745694 }} />}
+        { props.isMarkerShown && <Marker position={{ lat: 42.143704, lng: 24.749983 }} />}
+        { props.isMarkerShown && <Marker position={{ lat: 42.147811, lng: 24.747950 }} />}
+        { props.isMarkerShown && <Marker position={{ lat: 42.143623, lng: 24.750512 }} />}
+      </GoogleMap>
     ));
 
     return (
       <div id='map'>
-        <GoogleMapExample containerElement={ <div style={{ height: `100%`, width: `100%` }}/> } mapElement={ <div style={{ height: `100%` }}/> }/>
+        <MapComponent isMarkerShown containerElement={ <div style={{ height: `100%`, width: `100%` }}/> } mapElement={ <div style={{ height: `100%` }}/> }/>
       </div>
     );
   }
