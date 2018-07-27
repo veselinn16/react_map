5d5522e3ad9840a3a24ff4fb5a32b431c480b65a
veselinn16 <veselinn16@gmail.com>
2018-07-27 19:46:32 +0300

    Style list with locations

--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -10,6 +10,7 @@ class LocationsList extends Component {
   render() {
     return <div className="sidebar">
         <div className="locations">
+          <h2 className="locations__heading">Locations</h2>
           <ul className="locations__list">
             {this.props.locations.map(location => {
               return <li className="locations__list--location" key={location.id}>
