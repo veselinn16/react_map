334510497d970edf1c2a9e33744a28b44c066f63
veselinn16 <veselinn16@gmail.com>
2018-07-30 22:20:27 +0300

    Filter markers v.1

--- src/App.js
+++ src/App.js
@@ -99,8 +99,6 @@ class App extends Component {
       }
     ],
 
-    map: null,
-
     locations: [
       { lat: 42.146255, lng: 24.74878, name: 'City Artistic Gallery', id: 1 },
       { lat: 42.140469, lng: 24.74578, name: 'Singing Fountains', id: 2 },
@@ -112,67 +110,165 @@ class App extends Component {
     filterMarkers: []
   }
 
-  createMarkers = (map) => {
-    this.state.locations.map( location => {
-      let marker = new window.google.maps.Marker({
-        position: { lat: location.lat, lng: location.lng },
-        map: map,
-        title: location.name,
-        animation: window.google.maps.Animation.DROP,
-        id: location.id
+  loadMap = () => {
+      let map = new window.google.maps.Map(document.getElementById('map'), {
+        center: { lat: 42.144953, lng: 24.746555 },
+        zoom: 16,
+        styles: this.state.styles
       })
 
-      let infoWindow = new window.google.maps.InfoWindow({
-        content: marker.title
-      });
+      if (this.state.locations.length !== 0) {
+        this.state.locations.map(location => {
+          let marker = new window.google.maps.Marker({
+            position: { lat: location.lat, lng: location.lng },
+            map: map,
+            title: location.name,
+            animation: window.google.maps.Animation.DROP,
+            id: location.id
+          })
 
-      let click = 1;
-      marker.addListener('click', () => {
-        if (marker.getAnimation() !== null) {
-          marker.setAnimation(null);
-        } else {
-          marker.setAnimation(window.google.maps.Animation.BOUNCE)
-        }
-        if (click % 2 !== 0) {
-          infoWindow.open(map, marker)
-        } else {
-          infoWindow.close()
-        }
-        click += 1;
-      })
-      
-      this.setState(prevState => ({
-        markers: [...prevState.markers, marker],
-        filterMarkers: [...prevState.filterMarkers, marker]
-      }))
-    }) 
-  }
+          let infoWindow = new window.google.maps.InfoWindow({
+            content: marker.title
+          });
+
+          let click = 1;
+          marker.addListener('click', () => {
+            if (marker.getAnimation() !== null) {
+              marker.setAnimation(null);
+            } else {
+              marker.setAnimation(window.google.maps.Animation.BOUNCE)
+            }
+            if (click % 2 !== 0) {
+              infoWindow.open(map, marker)
+            } else {
+              infoWindow.close()
+            }
+            click += 1;
+          })
+
+          this.setState(prevState => ({
+            markers: [...prevState.markers, marker],
+            filterMarkers: [...prevState.filterMarkers, marker]
+          }))
+        })
+        this.setState({ locations: [] })
+      } else {
+        console.log(this.state.markers)
+        this.state.markers.map(marker => {
+          let newMarker = new window.google.maps.Marker({
+            position: marker.position,
+            map: map,
+            title: marker.title,
+            animation: window.google.maps.Animation.DROP,
+            id: marker.id
+          })
+
+          let infoWindow = new window.google.maps.InfoWindow({
+            content: marker.title
+          });
+
+          let click = 1;
+          newMarker.addListener('click', () => {
+            if (newMarker.getAnimation() !== null) {
+              newMarker.setAnimation(null);
+            } else {
+              newMarker.setAnimation(window.google.maps.Animation.BOUNCE)
+            }
+            if (click % 2 !== 0) {
+              infoWindow.open(map, newMarker)
+            } else {
+              infoWindow.close()
+            }
+            click += 1;
+          })
+        })
+      }
+    }
 
   filterLocations = (query) => {
     let filteredLocations = []
-    for(let marker of this.state.filterMarkers) {
-      if(marker.title.toLowerCase().includes(query.toLowerCase())) {
+    for (let marker of this.state.filterMarkers) {
+      if (marker.title.toLowerCase().includes(query.toLowerCase())) {
         filteredLocations.push(marker)
       }
     }
-    this.setState({markers: filteredLocations})
+    this.setState({ markers: filteredLocations })
+    this.loadMap()
   }
+
+  // createMarkers = (map) => {
+  //   if(this.state.locations.length !== 0) {
+  //   this.state.locations.map( location => {
+  //     let marker = new window.google.maps.Marker({
+  //       position: { lat: location.lat, lng: location.lng },
+  //       map: map,
+  //       title: location.name,
+  //       animation: window.google.maps.Animation.DROP,
+  //       id: location.id
+  //     })
+
+  //     let infoWindow = new window.google.maps.InfoWindow({
+  //       content: marker.title
+  //     });
+
+  //     let click = 1;
+  //     marker.addListener('click', () => {
+  //       if (marker.getAnimation() !== null) {
+  //         marker.setAnimation(null);
+  //       } else {
+  //         marker.setAnimation(window.google.maps.Animation.BOUNCE)
+  //       }
+  //       if (click % 2 !== 0) {
+  //         infoWindow.open(map, marker)
+  //       } else {
+  //         infoWindow.close()
+  //       }
+  //       click += 1;
+  //     })
+      
+  //     this.setState(prevState => ({
+  //       markers: [...prevState.markers, marker],
+  //       filterMarkers: [...prevState.filterMarkers, marker]
+  //     }))
+  //   }) 
+  //   this.setState({locations: []})
+  //   } else {
+  //     this.state.markers.map( marker => {
+  //       let newMarker = new window.google.maps.Marker({
+  //         position: marker.position,
+  //         map: map,
+  //         title: marker.title,
+  //         animation: window.google.maps.Animation.DROP,
+  //         id: marker.id
+  //       })
+
+  //       let infoWindow = new window.google.maps.InfoWindow({
+  //         content: marker.title
+  //       });
+
+  //       let click = 1;
+  //       newMarker.addListener('click', () => {
+  //         if (newMarker.getAnimation() !== null) {
+  //           newMarker.setAnimation(null);
+  //         } else {
+  //           newMarker.setAnimation(window.google.maps.Animation.BOUNCE)
+  //         }
+  //         if (click % 2 !== 0) {
+  //           infoWindow.open(map, newMarker)
+  //         } else {
+  //           infoWindow.close()
+  //         }
+  //         click += 1;
+  //       })
+  //     })
+  //   }
+  // }
 
   render() {
     return (
       <div className="App">
         <LocationsList markers={this.state.markers} filterLocations={this.filterLocations}/>
-        <Map id="map" options={{
-          center: { lat: 42.144953, lng: 24.746555 },
-          zoom: 16,
-          styles: this.state.styles
-        }} onMapLoad={ map => {
-          // sets state 'map' = the map
-          this.setState({map: map})
-
-          // maps the state 'locations' and creates markers with infoWindows and puts them on the map, after which sets the state 'markers' and 'filterMarkers' array equal to an array of the markers on the map
-          this.createMarkers(map);
-        }}/>
+        <Map id="map" loadMap={this.loadMap}/>
       </div>
     );
   }
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,33 +1,8 @@
 import React, { Component } from 'react';
 
 class Map extends Component {
-  constructor(props) {
-    super(props);
-    this.onScriptLoad = this.onScriptLoad.bind(this)
-  }
-
-  onScriptLoad() {
-    const map = new window.google.maps.Map(
-      document.getElementById(this.props.id),
-      this.props.options);
-    this.props.onMapLoad(map)
-  }
-
   componentDidMount() {
-    if(!window.google) {
-      let s = document.createElement('script');
-      s.type = 'text/javascript';
-      s.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyAXf7H3ihwgpM3XQWzrxQcSlk1ZCPTOgAU`;
-      let x = document.getElementsByTagName('script')[0];
-      x.parentNode.insertBefore(s, x);
-      
-      // Access google.maps when it's finished loading
-      s.addEventListener('load', e => {
-        this.onScriptLoad()
-      })
-    } else {
-      this.onScriptLoad()
-    }
+    this.props.loadMap()
   }
 
   render() {
