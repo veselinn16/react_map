af5edb6c9725615d9c345639c584761ae2b87ce9
veselinn16 <veselinn16@gmail.com>
2018-08-01 17:19:19 +0300

    Change 2 locations

--- src/App.js
+++ src/App.js
@@ -100,11 +100,11 @@ class App extends Component {
     ],
 
     locations: [
-      { lat: 42.146255, lng: 24.74878, name: 'City Artistic Gallery', id: 1 },
+      { lat: 42.148191, lng: 24.752817, name: 'Old Town Plovdiv', id: 1 },
       { lat: 42.140469, lng: 24.74578, name: 'Singing Fountains', id: 2 },
       { lat: 42.143726, lng: 24.750003, name: 'Hemingway', id: 3 },
       { lat: 42.147669, lng: 24.748052, name: 'Ancient Stadium of Philipopolis', id: 4 },
-      { lat: 42.143653, lng: 24.75042, name: 'Roman Odeon of Philipopolis', id: 5 }
+      { lat: 42.146886, lng: 24.751069, name: 'Ancient Theater of Philipopolis', id: 5 }
     ],
     map: null,
     markers: [],
@@ -157,6 +157,7 @@ class App extends Component {
         filterMarkers: [...prevState.filterMarkers, marker]
       }))
     })
+    // this.loadDataFromAPI()
   }
 
   filterLocations = (query) => {
