a1ef1bc4023e244f1d98f10ff4cceadbf68d6327
veselinn16 <veselinn16@gmail.com>
2018-08-02 20:49:56 +0300

    Add hamburger icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,9 @@ import React from 'react';
 const Header = () => {
   return (
     <header className="header">
+      <label htmlFor="header__menu-toggle" className="header__menu__button">
+        <span className="header__menu__icon">&nbsp;</span>
+      </label>
       <h1 className="header__heading">Welcome to Plovdiv</h1>
     </header>
   )
