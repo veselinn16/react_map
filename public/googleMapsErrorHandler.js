936e39b61f138f21c50dfcea2555ee2c9999bda0
veselinn16 <veselinn16@gmail.com>
2018-08-05 14:40:30 +0300

    Empty locations from locations list if there is no map

--- public/googleMapsErrorHandler.js
+++ public/googleMapsErrorHandler.js
@@ -1,3 +1,10 @@
 function gm_authFailure() {
   alert("There was a problem loading Google maps!");
+
+  // Empty locations from locations list if there is no map
+  let locationsList = document.getElementsByClassName('locations__list')
+  let locations = locationsList[0].children
+  for(let location of locations) {
+    location.hidden = true
+  }
 }
