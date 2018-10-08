83a402b8f1822bc0eb87516195b79465622f8dc9
veselinn16 <veselinn16@gmail.com>
2018-10-08 22:34:57 +0300

    Change button :sparkles:

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,16 @@ class Header extends Component {
   render() {
     return (
       <header className="header">
-        <label tabIndex="0" htmlFor="header__menu-toggle" className="header__menu__button" onClick={this.props.showSidebar}>
+        {/* <label tabIndex="0" htmlFor="header__menu-toggle" className="header__menu__button" onClick={this.props.showSidebar}>
           <span className="header__menu__icon">&nbsp;</span>
-        </label>
+        </label> */}
+        <div class="header__btn">
+          <div class="header__wrapper-menu" onClick={this.props.showSidebar}>
+            <div class="line__menu line__menu-half line__menu-start"></div>
+            <div class="line__menu"></div>
+            <div class="line__menu line__menu-half line__menu-end"></div>
+          </div>
+        </div>
         <h1 className="header__heading">Welcome to Plovdiv</h1>
       </header>
     )
