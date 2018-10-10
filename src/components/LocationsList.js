523a6dd26b7d98b23711c12ff5486cf88a4a2bd7
veselinn16 <veselinn16@gmail.com>
2018-10-11 00:05:22 +0300

    Remove image from page :fire:

--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+// import img from "../x3.png";
 
 const LocationsList = (props) => {
   const filterLocations = (event) => {
@@ -10,22 +11,30 @@ const LocationsList = (props) => {
   }
 
   return (
-    <div className='sidebar'>
+    <div className='sidebar sidebar__invisible'>
       <div className="locations">
         <h2 className="locations__heading">Locations</h2>
         <ul className="locations__list">
           {props.markers.map(marker => {
             return <li className="locations__list--location" tabIndex="0" onKeyPress={selectMarker} key={marker.id} onClick={selectMarker}>
-                {marker.title}
-              </li>;
+                {/* {marker.title} */}
+             </li>              
           })}
+          <li className="locations__list--location">Nebet Tepe</li>
+          <li className="locations__list--location">Singing Fountains</li>
+          <li className="locations__list--location">Basquiat Café & Boutique</li>
+          <li className="locations__list--location">Ancient Stadium of Philipopolis</li>
+          <li className="locations__list--location">Ancient Stadium of Philipopolis</li>
         </ul>
       </div>
-      <h2 className="filter__section--heading">Filter Locations</h2>
+      <h2 className="filter__section--heading">Locations Filter</h2>
       <div className="filter__section">
-        <input type="text" className="filter__section--input" placeholder="Filter Locations" role="search" id="filter" onChange={filterLocations}/>
+        <input type="text" className="filter__section--input" placeholder="filter locations..." role="search" id="filter" onChange={filterLocations}/>
         <label htmlFor="filter" className="filter__section--label">Filter Locations</label>
       </div>
+      <footer className="footer">Copyright &copy; 2018 Veselin Tonev. <br/>
+        All rights reserved.
+      </footer>
     </div>
   )
 }
