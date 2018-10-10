e9f09ffdc3918d77f271aa0ad25d0ba5ec7096f9
veselinn16 <veselinn16@gmail.com>
2018-10-11 00:25:01 +0300

    Add footer text :wrench:

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+    <footer className="footer">Copyright &copy; 2018 Veselin Tonev. <br/>
+        All rights reserved.
+    </footer>
+    )
+}
+
+export default Footer;
