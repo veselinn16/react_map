2362b3ab09d971337d975587ca13b3cca1223d56
veselinn16 <veselinn16@gmail.com>
2018-10-11 23:53:03 +0300

    Add attribution link :wrench:

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 const Footer = () => {
     return (
-    <footer className="footer">Copyright &copy; 2018 Veselin Tonev. <br/>
+    <footer className="footer">
+    Powered by <a className="footer__link" data-hover-label="Foursquare" href="https://developer.foursquare.com/"><span className="footer__link-label">Foursquare</span></a> <br/>
+    Copyright &copy; 2018 Veselin Tonev. <br/>
         All rights reserved.
     </footer>
     )
