920142da341114c64faec56b218a3c3c5cddd85d
veselinn16 <veselinn16@gmail.com>
2018-10-11 08:06:51 +0300

    Convert Filter to functional component :wrench:

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
 
-class Filter extends Component {
-  render() {
-    return (
-      <div className="filter">
-        <input type="form"/>
-      </div>
-    )
-  }
+const Filter = () => {
+  return (
+    <div className="filter">
+      <input type="form"/>
+    </div>
+  )
 }
 
 export default Filter
