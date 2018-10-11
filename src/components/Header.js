5efa270a29b57c9cac1ba5767930a5904efacc0e
veselinn16 <veselinn16@gmail.com>
2018-10-11 08:09:40 +0300

    Convert header to functional component :wrench:

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,18 @@
-import React, { Component} from 'react';
+import React from 'react';
 
-class Header extends Component {
-  render() {
-    return (
-      <header className="header">
-        <div className="header__btn">
-          <div className="header__wrapper-menu" onClick={this.props.showSidebar}>
-            <div className="line__menu half start"></div>
-            <div className="line__menu"></div>
-            <div className="line__menu half end"></div>
-          </div>
+const Header = (props) => {
+  return (
+    <header className="header">
+      <div className="header__btn">
+        <div className="header__wrapper-menu" onClick={props.showSidebar}>
+          <div className="line__menu half start"></div>
+          <div className="line__menu"></div>
+          <div className="line__menu half end"></div>
         </div>
-        <h1 className="header__heading">Welcome to Plovdiv</h1>
-      </header>
-    )
-  }
+      </div>
+      <h1 className="header__heading">Welcome to Plovdiv</h1>
+    </header>
+  )
 }
 
 export default Header
