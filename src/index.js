10b8b300d2eb3caa3eed4322bb2a0132db3280e2
veselinn16 <veselinn16@gmail.com>
2018-10-13 11:07:38 +0300

    Comment out serviceWorker script :wrench:

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './index.css';
 import "./App.css";
 import App from './App';
-import registerLocalSW from "./registerLocalSW.js";
+// import registerLocalSW from "./registerLocalSW.js";
 // import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
